a4449a6c1c338a7735f190233f56fb29e1513d12
Michal Trybus <komar007@gmail.com>
2010-01-05 12:44:38 +0100

    Fixed tabs, reorganized a bit, changed window title color, added fullscreen float to managehook

--- xorg/xmonad/xmonad.hs
+++ xorg/xmonad/xmonad.hs
@@ -1,6 +1,7 @@
-import qualified Data.Map as M
 import System.IO
+import qualified Data.Map as M
 import qualified System.IO.UTF8
+import Data.Ratio ((%))
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.DynamicHooks
@@ -30,7 +31,7 @@ import XMonad.Actions.GridSelect
 import XMonad.Actions.Promote
 import XMonad.Actions.NoBorders
 import XMonad.Actions.CycleWS
-import Data.Ratio ((%))
+import XMonad.Actions.SpawnOn
 
 browser = "firefox"
 
@@ -43,110 +44,112 @@ myWide = named "[-]" $ Mirror tall
 myFull = named "[F]" Full
 
 defaultSet =
-  myTall ||| myWide ||| myFull
+    myTall ||| myWide ||| myFull
 defaultMSet = 
-  myWide ||| myTall ||| myFull
+    myWide ||| myTall ||| myFull
 
 myLayoutHook = (workspaceDir "~") . smartBorders . avoidStruts $
-  onWorkspace "stats" stats $
-	defaultSet
-  where
-  stats = reflectHoriz $ withIM 0.125 (Resource "gkrellm") defaultMSet
---	stats = gaps [(R, 150)] defaultMSet
+    onWorkspace "stats" stats $
+    defaultSet
+    where
+    stats = reflectHoriz $ withIM 0.125 (Resource "gkrellm") defaultMSet
+--    stats = gaps [(R, 150)] defaultMSet
 
 
 myLogHook pipe = dynamicLogWithPP $ xmobarPP {
-  ppOutput = System.IO.UTF8.hPutStrLn pipe,
-  ppTitle = xmobarColor "#73d216" "" . shorten 100,
-  ppCurrent = xmobarColor "#c4a000" "" .  wrap "[" "]",
-  ppUrgent = xmobarColor "#dd0000" "",
-  ppSep = xmobarColor "#aaaaaa" "" " | ",
-	ppHidden = \s -> if s == "SP" then "" else s, -- FIXME: awful
-	ppVisible = wrap (xmobarColor "#c4a000" "" "[") (xmobarColor "#c4a000" "" "]")
+    ppOutput = System.IO.UTF8.hPutStrLn pipe,
+    ppTitle = xmobarColor "#3465a4" "" . shorten 100,  -- previously: "#73d216"
+    ppCurrent = xmobarColor "#c4a000" "" .  wrap "[" "]",
+    ppUrgent = xmobarColor "#dd0000" "",
+    ppSep = xmobarColor "#aaaaaa" "" " | ",
+    ppHidden = \s -> if s == "SP" then "" else s, -- FIXME: awful
+    ppVisible = wrap (xmobarColor "#c4a000" "" "[") (xmobarColor "#c4a000" "" "]")
 }
 
 myScratchpadManageHook = scratchpadManageHook(W.RationalRect 0.25 0.33 0.5 0.33) 
 myManageHook = myScratchpadManageHook <+> myConditions <+> manageDocks <+> manageHook defaultConfig
 myConditions = composeAll [
-  resource  =? "gkrellm"    --> (ask >>= doF . W.sink),
-	resource  =? "gkrellm"    --> doF (W.shift "stats"),
-	resource  =? "gkrellm"    --> (ask >>= \w -> liftX (toggleBorder w) >> doF id),
-	resource  =? "stats"      --> doF (W.shift "stats"),  
-	resource  =? "irc"        --> doF (W.shift "irc"),
-	className =? "Gajim.py"   --> doF (W.shift "im")]
+    resource  =? "gkrellm"    --> (ask >>= doF . W.sink),
+    resource  =? "gkrellm"    --> doF (W.shift "stats"),
+    resource  =? "gkrellm"    --> (ask >>= \w -> liftX (toggleBorder w) >> doF id),
+    resource  =? "stats"      --> doF (W.shift "stats"),  
+    resource  =? "irc"        --> doF (W.shift "irc"),
+    isFullscreen              --> doFullFloat,
+    className =? "Gajim.py"   --> doF (W.shift "im")]
 
 xpconfig = defaultXPConfig {
-  font        = "-misc-fixed-*-*-*-*-10-*-*-*-*-*-*-*",
-  bgColor     = "#000000",
-  fgColor     = "#aaaaaa",
-  bgHLight    = "#cccccc",
-  fgHLight    = "#000000",
-  promptBorderWidth = 0,
-  height = 10
+    font        = "-misc-fixed-*-*-*-*-10-*-*-*-*-*-*-*",
+    bgColor     = "#000000",
+    fgColor     = "#aaaaaa",
+    bgHLight    = "#cccccc",
+    fgHLight    = "#000000",
+    promptBorderWidth = 0,
+    height = 10
 }
 
 floatSearchResult dhRef = oneShotHook dhRef (className =? "Firefox") (doRectFloat $ W.RationalRect 0.15 0.15 0.7 0.7)
 
-myConf xmproc dynHooksRef = defaultConfig {
-  manageHook = myManageHook <+> dynamicMasterHook dynHooksRef,
-  layoutHook = myLayoutHook,
-  logHook = myLogHook xmproc,
-  focusFollowsMouse = False,
-  modMask = mod4Mask,
-  normalBorderColor = "#000000",
-  focusedBorderColor = "#3465a4",
-  terminal = "urxvt",
-  workspaces = ["c1", "d1", "c2", "d2", "c3", "d3", "sys1", "ds1", "sys2", "ds2", "web1", "web2", "web3", "web4", "web5", "im", "irc", "temp", "stats"]
-} `additionalKeysP` (myKeys dynHooksRef xmproc) `additionalKeys` myKeysMulti
+myConf spawner xmproc dynHooksRef = defaultConfig {
+    manageHook = manageSpawn spawner <+> myManageHook <+> dynamicMasterHook dynHooksRef,
+    layoutHook = myLayoutHook,
+    logHook = myLogHook xmproc,
+    focusFollowsMouse = False,
+    modMask = mod4Mask,
+    normalBorderColor = "#000000",
+    focusedBorderColor = "#3465a4",
+    terminal = "urxvt",
+    workspaces = ["c1", "d1", "c2", "d2", "c3", "d3", "sys1", "ds1", "sys2", "ds2", "web1", "web2", "web3", "web4", "web5", "im", "irc", "temp", "stats"]
+} `additionalKeysP` (myKeys dynHooksRef xmproc spawner) `additionalKeys` myKeysMulti
 
 main = do
-  xmproc <- spawnPipe "/usr/bin/xmobar /home/komar/.xmonad/xmobar2"
-  xmproc2 <- spawnPipe "/usr/bin/xmobar /home/komar/.xmonad/xmobar1"
-  dynHooksRef <- initDynamicHooks
-  xmonad $ withUrgencyHook NoUrgencyHook $ myConf xmproc dynHooksRef
+    sp <- mkSpawner
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/komar/.xmonad/xmobar2"
+    xmproc2 <- spawnPipe "/usr/bin/xmobar /home/komar/.xmonad/xmobar1"
+    dynHooksRef <- initDynamicHooks
+    xmonad $ withUrgencyHook NoUrgencyHook $ myConf sp xmproc dynHooksRef
 
-dmenu_opts = "dmenu -fn -misc-fixed-*-*-*-*-10-*-*-*-*-*-*-* -b -nb '#000000' -nf '#aaaaaa' -sb '#204a87' -sf '#aaaaaa'"
-dmenu_exe = "exe=`dmenu_path | " ++ dmenu_opts ++  "` && $exe"
+-- dmenu_opts = "dmenu -fn -misc-fixed-*-*-*-*-10-*-*-*-*-*-*-* -b -nb '#000000' -nf '#aaaaaa' -sb '#204a87' -sf '#aaaaaa'"
+-- dmenu_exe = "exe=`dmenu_path | " ++ dmenu_opts ++  "` && $exe"
 
 workspaceKeys = ["M-1", "M-<F1>", "M-2", "M-<F2>", "M-3", "M-<F3>", "M-4", "M-<F4>", "M-5", "M-<F5>",
-	"M-6", "M-7", "M-8", "M-9", "M-0", "M--", "M-i", "M-=", "M-\\"]
+    "M-6", "M-7", "M-8", "M-9", "M-0", "M--", "M-i", "M-=", "M-\\"]
 workspaceSKeys = map ("S-"++) workspaceKeys
 
-myKeys dhRef xmproc = [
-  ("M-`",            workspacePrompt xpconfig (windows . W.view)),
-  ("M-S-`",          workspacePrompt xpconfig (windows . W.shift)),
-  ("M-p",            spawn (dmenu_exe)),
-  ("M-s",            scratchpadSpawnActionTerminal "urxvt"),
-  ("M-d",            changeDir xpconfig),
-  ("M-S-z",          floatSearchResult dhRef >> (selectSearchBrowser browser mySearchEngine)),
-  ("M-z",            floatSearchResult dhRef >> (promptSearchBrowser xpconfig browser mySearchEngine)),
-	("M-<Esc>",        goToSelected defaultGSConfig),
-	("M-<Return>",     promote),
-	("M-S-<Backspace>",focusUrgent),
-	("M-<Backspace>",  toggleWS)]
-	++
-	[(sc, withNthWorkspace W.greedyView n) | (sc, n) <- zip workspaceKeys [0..]]
-	++
-	[(sc, withNthWorkspace W.shift n) | (sc, n) <- zip  workspaceSKeys [0..]]
-	++
-	[(mod ++ sc, func (Lines 1) Linear dir) |
-		(sc, dir) <- [("[", ToLeft), ("]", ToRight)],
-		(mod, func) <- [("M-", planeMove), ("M-S-", planeShift)]]
+myKeys dhRef xmproc spawner = [
+    ("M-`",               workspacePrompt xpconfig (windows . W.view)),
+    ("M-S-`",             workspacePrompt xpconfig (windows . W.shift)),
+    ("M-p",               shellPromptHere spawner xpconfig),
+    ("M-s",               scratchpadSpawnActionTerminal "urxvt"),
+    ("M-d",               changeDir xpconfig),
+    ("M-S-z",             floatSearchResult dhRef >> (selectSearchBrowser browser mySearchEngine)),
+    ("M-z",               floatSearchResult dhRef >> (promptSearchBrowser xpconfig browser mySearchEngine)),
+    ("M-<Esc>",           goToSelected defaultGSConfig),
+    ("M-<Return>",        promote),
+    ("M-S-<Backspace>",   focusUrgent),
+    ("M-<Backspace>",     toggleWS)]
+    ++
+    [(sc, withNthWorkspace W.greedyView n) | (sc, n) <- zip workspaceKeys [0..]]
+    ++
+    [(sc, withNthWorkspace W.shift n) | (sc, n) <- zip  workspaceSKeys [0..]]
+    ++
+    [(mod ++ sc, func (Lines 1) Linear dir) |
+        (sc, dir) <- [("[", ToLeft), ("]", ToRight)],
+        (mod, func) <- [("M-", planeMove), ("M-S-", planeShift)]]
 
 mySearchEngine = searchEngineF "multi" parseGoogle 
-	where
-	parseGoogle s | s == "gmail" = "http://gmail.com"
-	              | otherwise    = (use mymulti) s
-	mymulti = intelligent multi
+    where
+    parseGoogle s | s == "gmail" = "http://gmail.com"
+                  | otherwise    = (use mymulti) s
+    mymulti = intelligent multi
 
 myKeysMulti = [
-  ((0,                    xK_XF86AudioPlay        ), spawn ("mpc toggle")),
-  ((0,                    xK_XF86AudioStop        ), spawn ("mpc stop")),
-  ((0,                    xK_XF86AudioPrev        ), spawn ("mpc prev")),
-  ((0,                    xK_XF86AudioNext        ), spawn ("mpc next")),
-  ((0,                    xK_XF86AudioMute        ), spawn ("amixer -q set Master toggle")),
-  ((0,                    xK_XF86AudioLowerVolume ), spawn ("amixer set Master\\ Front 2dB-")),
-  ((0,                    xK_XF86AudioRaiseVolume ), spawn ("amixer set Master\\ Front 2dB+"))]
+    ((0,                    xK_XF86AudioPlay        ), spawn ("mpc toggle")),
+    ((0,                    xK_XF86AudioStop        ), spawn ("mpc stop")),
+    ((0,                    xK_XF86AudioPrev        ), spawn ("mpc prev")),
+    ((0,                    xK_XF86AudioNext        ), spawn ("mpc next")),
+    ((0,                    xK_XF86AudioMute        ), spawn ("amixer -q set Master toggle")),
+    ((0,                    xK_XF86AudioLowerVolume ), spawn ("amixer set Master\\ Front 2dB-")),
+    ((0,                    xK_XF86AudioRaiseVolume ), spawn ("amixer set Master\\ Front 2dB+"))]
 
 xK_XF86AudioLowerVolume = 0x1008ff11
 xK_XF86AudioMute = 0x1008ff12
