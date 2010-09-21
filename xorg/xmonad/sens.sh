ea19f90d6719e2f29b184860d17af5c33c0f16ae
Michal Trybus <komar007@gmail.com>
2010-09-21 22:10:52 +0200

    fixed multiple-line problem in sens.sh

--- xorg/xmonad/sens.sh
+++ xorg/xmonad/sens.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sensors | awk -F: "/$1/{print \$2}" | awk -F\( '//{print $1}' | egrep '[0-9]+' -o
+sensors | awk -F: "/$1/{print \$2}" | awk -F\( '//{print $1}' | egrep '[0-9]+' -o | head -n 1
