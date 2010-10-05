9cb9d232d2b84038552685082bb9b0cc9b15d414
Michal Trybus <komar007@gmail.com>
2010-10-05 15:24:41 +0200

    minor in laptop

--- xorg/xmonad/xmonad.hs
+++ xorg/xmonad/xmonad.hs
@@ -143,7 +143,7 @@ myConf spawner xmproc dynHooksRef = defaultConfig {
 
 main = do
     sp <- mkSpawner
-    xmproc <- spawnPipe $ "./panel_launch.sh"
+    xmproc <- spawnPipe $ "sh -c ~/.xmonad/panel_launch.sh"
     dynHooksRef <- initDynamicHooks
     xmonad $ withUrgencyHook NoUrgencyHook $ myConf sp xmproc dynHooksRef
 
