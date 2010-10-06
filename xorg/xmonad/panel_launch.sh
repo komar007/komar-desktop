1cf2f41a5534d69e7dd3bec9fe46c1baa2dbbea4
Michal Trybus <komar007@gmail.com>
2010-10-06 12:59:35 +0200

    fixed laptop xmobar

--- xorg/xmonad/panel_launch.sh
+++ xorg/xmonad/panel_launch.sh
@@ -1,6 +1,14 @@
+if [[ -a /proc/acpi/battery/BAT0/ ]]; then
+	CONFIG=laptop
+else
+	CONFIG=desktop
+fi
+
 FIFO=/tmp/xmobar-panel-fifo
 mkfifo $FIFO
 /usr/bin/dzen2 -bg black -xs 2 -ta l -fn '-misc-fixed-*-*-*-*-10-*-*-*-*-*-*-*' < $FIFO &
-/usr/bin/xmobar '/home/komar/.xmonad/xmobar-desktop' &
-/usr/bin/xmobar '/home/komar/.xmonad/xmobar-info' &
+/usr/bin/xmobar "/home/komar/.xmonad/xmobar-$CONFIG" &
+if [[ $CONFIG == desktop ]]; then
+    /usr/bin/xmobar "/home/komar/.xmonad/xmobar-info"
+fi &
 cat > $FIFO
