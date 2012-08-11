7898f8197c8f13470aa933f584970a7bff9291a0
Michal Trybus <komar007@gmail.com>
2012-08-11 20:51:54 +0200

    changed blinking timings

--- xorg/xmonad/blink.sh
+++ xorg/xmonad/blink.sh
@@ -5,12 +5,12 @@ rm -f /tmp/noblink
 while [ true ]; do
         for i in `seq 1 1 3`; do
                 xset led 3
-                sleep 0.2
+                sleep 0.1
                 xset -led 3
                 sleep 0.2
         done
         if [ -f /tmp/noblink ]; then
                 break;
         fi
-        sleep 0.8
+        sleep 1.1
 done
