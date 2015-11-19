cd53437a921697d3dc08734c54e6ec68b2bd4bf9
Michal Trybus <komar007@gmail.com>
2015-11-19 20:07:51 +0100

    [gdb] prompt enhancements

---
+++ gdb/gdb/prompt.py
@@ -0,0 +1,23 @@
+import gdb
+from gdb.prompt import substitute_prompt
+
+p_tmpl = r'\[\e[01;31m\]gdb\[\e[01;32m\][\[\e[0m\]{thread}\[\e[01;32m\]] \[\e[0m\]{frame}\[\e[01;34m\]$ \[\e[0m\]'
+
+def render_prompt(cur):
+    try:
+        frame = gdb.selected_frame()
+        f_str = "%s " % frame.name()
+    except gdb.error:
+        f_str = ""
+
+    t_str = ""
+    try:
+        thread = gdb.selected_thread()
+        if thread is not None:
+            t_str = "{num}: {name}".format(num=thread.num, name=thread.name)
+    except gdb.error:
+        pass
+
+    return substitute_prompt(p_tmpl.format(thread=t_str, frame=f_str))
+
+gdb.prompt_hook = render_prompt
