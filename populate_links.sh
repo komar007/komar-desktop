bb9a7668606a072d383945ba98a630ca3ab9c2a5
Michal Trybus <M.Trybus@adbglobal.com>
2015-11-19 10:17:11 +0100

    added gdb config

--- populate_links.sh
+++ populate_links.sh
@@ -3,7 +3,7 @@
 if [ -f ~/.bashrc ]; then
 	mv ~/.bashrc ~/.bashrc_orig
 fi
-for d in xorg vim bash mutt; do
+for d in xorg vim bash mutt gdb; do
 	for f in $d/*; do
 		ln -s `pwd`/$f ~/.`basename $f`
 	done
