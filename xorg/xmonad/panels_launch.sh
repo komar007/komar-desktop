04a2f1c99238a9ebf7959f08cab463e72356f3d3
Michal Trybus <M.Trybus@adbglobal.com>
2017-01-10 15:29:01 +0100

    [panels] added graphics + small refactoring

--- xorg/xmonad/panels_launch.sh
+++ xorg/xmonad/panels_launch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ICON_ROOT="$HOME/.xmonad/dzen2_img/"
+ICON_OFFSET=3
+
 if [[ -a ~/.desktop_type ]]; then
 	CONFIG=`cat ~/.desktop_type`
 elif [[ -a /proc/acpi/battery/BAT0/ ]]; then
@@ -23,13 +26,15 @@ while true; do
 done | /usr/bin/dzen2 -bg black -xs 2 -ta l -fn '-misc-fixed-*-*-*-*-10-*-*-*-*-*-*-*' &
 sleep 0.1
 if [[ $CONFIG == desktop ]]; then
-    /usr/bin/xmobar ~/.xmonad/xmobar-info-desktop &
-    cpp -DPOS=2360 -DWIDTH=200 ~/.xmonad/xmobar-clock.in | sed '/^#/d' > /tmp/xmobar-clock
+    cpp -P -DICON_OFFSET=3 -DICON_ROOT=\"$ICON_ROOT\" ~/.xmonad/xmobar-info-desktop.in > /tmp/xmobar-info-desktop
+    /usr/bin/xmobar /tmp/xmobar-info-desktop &
+    cpp -P -DICON_OFFSET=3 -DICON_ROOT=\"$ICON_ROOT\" -DPOS=2360 -DWIDTH=200 ~/.xmonad/xmobar-clock.in > /tmp/xmobar-clock
     /usr/bin/xmobar /tmp/xmobar-clock &
 elif [[ $CONFIG == work ]]; then
-    /usr/bin/xmobar ~/.xmonad/xmobar-info-work &
-    cpp -DPOS=1000 -DWIDTH=200 -DSIDE_LEFT  ~/.xmonad/xmobar-clock.in | sed '/^#/d' > /tmp/xmobar-clock1
-    cpp -DPOS=2630 -DWIDTH=250 -DSIDE_RIGHT ~/.xmonad/xmobar-clock.in | sed '/^#/d' > /tmp/xmobar-clock2
+    cpp -P -DICON_OFFSET=3 -DICON_ROOT=\"$ICON_ROOT\" ~/.xmonad/xmobar-info-work.in > /tmp/xmobar-info-work
+    /usr/bin/xmobar /tmp/xmobar-info-work &
+    cpp -P -DICON_OFFSET=3 -DICON_ROOT=\"$ICON_ROOT\" -DPOS=1000 -DWIDTH=200 -DSIDE_LEFT  ~/.xmonad/xmobar-clock.in > /tmp/xmobar-clock1
+    cpp -P -DICON_OFFSET=3 -DICON_ROOT=\"$ICON_ROOT\" -DPOS=2630 -DWIDTH=250 -DSIDE_RIGHT ~/.xmonad/xmobar-clock.in > /tmp/xmobar-clock2
     /usr/bin/xmobar /tmp/xmobar-clock1 &
     /usr/bin/xmobar /tmp/xmobar-clock2 &
 elif [[ $CONFIG == laptop ]]; then
