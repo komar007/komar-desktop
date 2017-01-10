a3e7f0f2d2967c429a6d008d31a2739833bb11b0
Michal Trybus <M.Trybus@adbglobal.com>
2017-01-10 12:10:17 +0100

    [lock] switched from slock to xtrlock

--- slock/slock_wrapper.sh
+++ slock/slock_wrapper.sh
@@ -5,5 +5,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LOGFILE="$DIR/slock_wrapper.log"
 
 echo "$(date +%s) lock"   >> "$LOGFILE"
-slock
+xtrlock
 echo "$(date +%s) unlock" >> "$LOGFILE"
