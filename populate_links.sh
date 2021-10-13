b94c3e5cf18793b47e8c1f312f66c839d5422fc6
Michal Trybus <M.Trybus@adbglobal.com>
2021-10-13 08:48:28 +0200

    [git] .git_template with ctags hooks

--- populate_links.sh
+++ populate_links.sh
@@ -8,6 +8,8 @@ for d in xorg vim bash mutt gdb tig; do
 		ln -s `pwd`/$f ~/.`basename $f`
 	done
 done
+ln -s `pwd`/git/gitconfig ~/.gitconfig
+ln -s `pwd`/git/git_template ~/.git_template
 ln -s `pwd`/slock ~/.slock
 mkdir -f ~/.icons
 ln -s `pwd`/icons/Neutral_Plus ~/.icons/
