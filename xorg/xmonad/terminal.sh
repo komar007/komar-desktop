3f6fbae501ea85c763159b9a028ad6fc5f460aff
Michal Trybus <M.Trybus@adbglobal.com>
2022-02-21 07:13:21 +0100

    [X] selectable terminal emulator

--- xorg/xmonad/terminal.sh
+++ xorg/xmonad/terminal.sh
@@ -1,4 +1,18 @@
+#!/bin/sh
+
+T=alacritty
+
 NAME=$1
 shift
-#urxvt -name "$NAME" -e "$@"
-alacritty --class "$NAME" "$@"
+
+if [ "$T" = alacritty ]; then
+        alacritty --class "$NAME" "$@"
+elif [ "$T" = urxvt ]; then
+        if [ $# -gt 0 ]; then
+                urxvt -name "$NAME" -e "$@"
+        else
+                urxvt -name "$NAME"
+        fi
+else
+        xterm -name "$NAME" "$@"
+fi
