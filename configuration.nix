8ca00090e54d8d2e38810b3526eff9fb957e82b4
Michal Trybus <komar007@gmail.com>
2024-07-14 11:14:13 +0200

    formatted configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,81 +1,112 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, ... }:
-
-{
+{ config
+, pkgs
+, ...
+}: {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  networking.networkmanager.enable = true;
+  services.logind.extraConfig = ''
+    HandlePowerKey=suspend
+  '';
 
   time.timeZone = "Europe/Warsaw";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "pl_PL.UTF-8";
+      LC_IDENTIFICATION = "pl_PL.UTF-8";
+      LC_MEASUREMENT = "pl_PL.UTF-8";
+      LC_MONETARY = "pl_PL.UTF-8";
+      LC_NAME = "pl_PL.UTF-8";
+      LC_NUMERIC = "pl_PL.UTF-8";
+      LC_PAPER = "pl_PL.UTF-8";
+      LC_TELEPHONE = "pl_PL.UTF-8";
+      LC_TIME = "pl_PL.UTF-8";
+    };
+  };
+  console.keyMap = "pl2";
 
-  i18n.defaultLocale = "en_US.UTF-8";
+  networking = {
+    hostName = "nixos";
+    networkmanager.enable = true;
+  };
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pl_PL.UTF-8";
-    LC_IDENTIFICATION = "pl_PL.UTF-8";
-    LC_MEASUREMENT = "pl_PL.UTF-8";
-    LC_MONETARY = "pl_PL.UTF-8";
-    LC_NAME = "pl_PL.UTF-8";
-    LC_NUMERIC = "pl_PL.UTF-8";
-    LC_PAPER = "pl_PL.UTF-8";
-    LC_TELEPHONE = "pl_PL.UTF-8";
-    LC_TIME = "pl_PL.UTF-8";
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
   };
 
   services.xserver = {
     enable = true;
-    videoDrivers = ["intel"];
+    videoDrivers = [ "intel" ];
     displayManager.startx.enable = true;
     xkb = {
       layout = "pl";
       variant = "";
     };
   };
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+    extraPackages = with pkgs; [
+      intel-media-sdk
+      intel-media-driver
+      libvdpau-va-gl
+    ];
+  };
+  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
 
-  # Configure console keymap
-  console.keyMap = "pl2";
-
-  # Bluetooth
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
-
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
     pulse.enable = true;
-    extraConfig.pipewire-pulse = {
-      "10-auto-ocnnect" = {
-        "pulse.cmd" = [
-          { "cmd" = "load-module"; "args" = "module-switch-on-connect"; }
-        ];
-      };
+    extraConfig.pipewire-pulse."10-auto-connect" = {
+      "pulse.cmd" = [
+        { cmd = "load-module"; args = "module-switch-on-connect"; }
+      ];
     };
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    wget
+    neovim
+    git
+    tig
+    killall
+    bc
+    zip
+
+    pciutils
+
+    openconnect
+  ];
+
+  fonts.packages = with pkgs; [
+    nerdfonts
+  ];
+
   users.users.komar = {
     isNormalUser = true;
     description = "Michał Trybus";
@@ -101,62 +132,20 @@
       gnumake
       cmake
       gcc
-
       rustup
 
       lua-language-server
       nixd
-      alejandra
+      nixpkgs-fmt
 
       mpv
-      ifwifi
       super-slicer-latest
 
       imagemagick
     ];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  environment.systemPackages = with pkgs; [
-    wget
-    neovim
-    git
-    tig
-    killall
-    bc
-    zip
-
-    pciutils
-
-    openconnect
-  ];
-
-  fonts.packages = with pkgs; [
-    nerdfonts
-  ];
-
-  nixpkgs.config.packageOverrides = pkgs: {
-    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
-  };
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    extraPackages = with pkgs; [
-      intel-media-sdk
-      intel-media-driver
-      libvdpau-va-gl
-    ];
-  };
-  environment.sessionVariables = { LIBVA_DRIVER_NAME = "iHD"; };
-
   services.openssh.enable = true;
-
-  services.logind.extraConfig = ''
-    HandlePowerKey=suspend
-  '';
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
