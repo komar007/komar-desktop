7522102b68d8b79f191e86d3a49acc0f82b12510
Michal Trybus <komar007@gmail.com>
2024-08-12 18:17:30 +0200

    (re)connecting bt devices after start pipewire/unsuspend

--- configuration.nix
+++ configuration.nix
@@ -30,6 +30,40 @@
     HandlePowerKey=suspend
   '';
 
+  systemd =
+    let
+      reconnect-bt = pkgs.writeShellScriptBin "reconnect-bt" ''
+        BT="${pkgs.bluez}/bin/bluetoothctl"
+        for _ in $(seq 10); do
+          if [ "$("$BT" list | wc -l)" -gt 0 ]; then
+            for dev in $("$BT" devices Trusted | cut -d' ' -f2); do
+              "$BT" -t1 connect "$dev"
+            done
+            echo "attempted to reconnect trusted bluetooth devices"
+            exit
+          fi
+          sleep 1
+        done
+        echo "failed to reconnect trusted bluetooth devices, no controller"
+      '';
+    in
+    {
+      user.services.connect-bt = {
+        description = "Connect trusted Bluetooth devices";
+        wantedBy = [ "pipewire.service" ];
+        after = [ "pipewire.service" ];
+        serviceConfig.Type = "oneshot";
+        script = "${pkgs.lib.getExe reconnect-bt}";
+      };
+      services.reconnect-bt = {
+        description = "Reconnect trusted Bluetooth devices";
+        wantedBy = [ "post-resume.target" ];
+        after = [ "post-resume.target" ];
+        serviceConfig.Type = "oneshot";
+        script = "${pkgs.lib.getExe reconnect-bt}";
+      };
+    };
+
   time.timeZone = "Europe/Warsaw";
   i18n = {
     defaultLocale = "en_US.UTF-8";
