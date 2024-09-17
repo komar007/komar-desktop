899b1e21181b1f87652916c2c4c28217c3cc0985
Michal Trybus <M.Trybus@adbglobal.com>
2024-09-17 07:32:15 +0200

    [work] slock dpms off

--- slock/slock_wrapper.sh
+++ slock/slock_wrapper.sh
@@ -5,5 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 LOGFILE="$DIR/slock_wrapper.log"
 
 echo "$(date +%s) lock"   >> "$LOGFILE"
+xset dpms force off
 xtrlock
 echo "$(date +%s) unlock" >> "$LOGFILE"
