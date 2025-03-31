30c03c2089e94b146ce3f08bfe83becf3ad64c35
Michal Trybus <komar007@gmail.com>
2025-03-31 21:15:51 +0200

    [home] initial firefox config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -69,4 +69,33 @@ in
     # Don't ever change this after the first build. Don't ask questions.
     stateVersion = "23.11";
   };
+
+  programs.firefox = {
+    enable = true;
+    profiles."default-release" = {
+      id = 0;
+      path = "default";
+      isDefault = true;
+
+      search.engines."Nix Packages" = {
+        definedAliases = [ "@np" ];
+        urls = [{
+          template = "https://search.nixos.org/packages";
+          params = [
+            { name = "query"; value = "{searchTerms}"; }
+          ];
+        }];
+        icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+      };
+      search.engines."Nix Options" = {
+        definedAliases = [ "@no" ];
+        urls = [{
+          template = "https://search.nixos.org/options";
+          params = [
+            { name = "query"; value = "{searchTerms}"; }
+          ];
+        }];
+      };
+    };
+  };
 }
