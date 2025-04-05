936527199099c54f0809cf5e0416e5e9b08ce794
Michal Trybus <komar007@gmail.com>
2025-04-05 17:08:48 +0200

    [firefox] reformatted slightly

--- homes/modules/firefox.nix
+++ homes/modules/firefox.nix
@@ -1,34 +1,37 @@
-{ lib, pkgs, ... }: {
-  programs.firefox = {
-    enable = true;
-    profiles."default-release" = {
-      id = 0;
-      path = "default";
-      isDefault = true;
+{ lib, pkgs, ... }:
+let
+  # stuff coming
+in
+{
+  programs.firefox.enable = true;
+  programs.firefox.profiles."default-release" = {
+    id = 0;
+    path = "default";
+    isDefault = true;
 
-      settings = {
-        "ui.key.menuAccessKeyFocuses" = false;
-      };
+    settings = {
+      "ui.key.menuAccessKeyFocuses" = false;
+    };
 
-      search.engines."Nix Packages" = {
-        definedAliases = [ "@np" ];
-        urls = [{
-          template = "https://search.nixos.org/packages";
-          params = [
-            { name = "query"; value = "{searchTerms}"; }
-          ];
-        }];
-        icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-      };
-      search.engines."Nix Options" = {
-        definedAliases = [ "@no" ];
-        urls = [{
-          template = "https://search.nixos.org/options";
-          params = [
-            { name = "query"; value = "{searchTerms}"; }
-          ];
-        }];
-      };
+    search.force = true;
+    search.engines."Nix Packages" = {
+      definedAliases = [ "@np" ];
+      urls = [{
+        template = "https://search.nixos.org/packages";
+        params = [
+          { name = "query"; value = "{searchTerms}"; }
+        ];
+      }];
+      icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+    };
+    search.engines."Nix Options" = {
+      definedAliases = [ "@no" ];
+      urls = [{
+        template = "https://search.nixos.org/options";
+        params = [
+          { name = "query"; value = "{searchTerms}"; }
+        ];
+      }];
     };
   };
 }
