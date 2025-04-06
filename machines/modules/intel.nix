990982a9b0d41c38d6ecf6d6c7381d60627f1101
Michal Trybus <komar007@gmail.com>
2025-04-06 11:32:37 +0200

    moved patched no-tearing xserver from intel to xservere module (not specific to intel)

--- machines/modules/intel.nix
+++ machines/modules/intel.nix
@@ -13,17 +13,4 @@
       libvdpau-va-gl
     ];
   };
-
-  system.replaceDependencies.replacements = [{
-    original = pkgs.xorg.xorgserver;
-    replacement = pkgs.xorg.xorgserver.overrideAttrs (old: {
-      patches = old.patches ++ [
-        (pkgs.fetchpatch
-          {
-            url = "https://github.com/komar007/xserver/compare/xorg-server-21.1.16...xorg-server-21.1.16_no_tear.patch";
-            sha256 = "sha256-zihVEOHX7BjlMjPu9WwyAUBnd5JWViuJ2jrtwC6Dbfc=";
-          })
-      ];
-    });
-  }];
 }
