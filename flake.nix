63c05d58cf8941ec025f3fbd95c23f0019c32d33
Michal Trybus <M.Trybus@adbglobal.com>
2025-04-07 12:13:12 +0200

    [hm] wrapped graphical packages with nixGL in work config using nixpkgs overlay

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixgl = {
+      url = "github:nix-community/nixGL";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nur = {
       url = "github:nix-community/NUR";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -21,7 +25,10 @@
       system = "x86_64-linux";
       nixpkgs-stable = system: import inputs.nixpkgs {
         inherit system;
-        overlays = [ inputs.nur.overlays.default ];
+        overlays = [
+          inputs.nur.overlays.default
+          inputs.nixgl.overlay
+        ];
       };
       nixpkgs-unstable = system: import inputs.nixpkgs-unstable {
         inherit system;
--- homes/work/default.nix
+++ homes/work/default.nix
@@ -4,6 +4,20 @@
     ../modules/xmonad.nix
   ];
 
+  nixpkgs.overlays = let
+    wrapWith = wrapper: pkg: pkgs.writeShellApplication {
+      name = "${pkg.meta.mainProgram}";
+      text = ''${pkgs.lib.getExe wrapper} ${pkgs.lib.getExe pkg} "$@"'';
+    };
+    nixGlIntelize = wrapWith pkgs.nixgl.nixGLIntel;
+  in [
+    (final: prev: {
+      # TODO: why does config.lib.nixGL.wrap cause infinite recursion in overlay?
+      alacritty = nixGlIntelize prev.alacritty;
+      mpv = nixGlIntelize prev.mpv;
+    })
+  ];
+
   home.packages = with pkgs; [
     mpv
     geeqie
