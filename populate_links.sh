6477852ccb6391318cf1f32c1853dcc8607ad932
Michal Trybus <komar007@gmail.com>
2025-05-03 21:10:37 +0200

    refactor: refreshed populate_links.sh - removed shellcheck/bashls warnings

--- populate_links.sh
+++ populate_links.sh
@@ -3,9 +3,12 @@
 if [ -f ~/.bashrc ]; then
 	mv ~/.bashrc ~/.bashrc_orig
 fi
+
+# The directories below contain legacy (non-home-manager) configuration files
+# They must be linked manually or with this script
 for d in xorg bash gdb; do
-	for f in $d/*; do
-		ln -s `pwd`/$f ~/.`basename $f`
+	for f in "$d"/*; do
+		ln -s "$(pwd)/$f" "$HOME/.$(basename "$f")"
 	done
 done
-ln -s `pwd`/slock ~/.slock
+ln -s "$(pwd)/slock" ~/.slock
