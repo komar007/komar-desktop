c522c8c600e8ec9383c93c2fe414403fd102823a
Michal Trybus <komar007@gmail.com>
2025-05-03 18:50:33 +0200

    chore: removed obsolete disused configs

--- populate_links.sh
+++ populate_links.sh
@@ -3,7 +3,7 @@
 if [ -f ~/.bashrc ]; then
 	mv ~/.bashrc ~/.bashrc_orig
 fi
-for d in xorg vim bash mutt gdb tig; do
+for d in xorg bash gdb tig; do
 	for f in $d/*; do
 		ln -s `pwd`/$f ~/.`basename $f`
 	done
